b536e3057ae8303e44788eed23da05604970d193
rafiina <14363298+rafiina@users.noreply.github.com>
2020-06-28 02:45:35 -0500

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const BOARD_WIDTH = 8;
+export const BOARD_HEIGHT = 8;
+export const ARMY_ROWS = 2;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as CONSTANTS from './constants.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+function Square({piece, row, column}) {
+    return (
+        <button className={row % 2 === column %2 ? "square-red" : "square-black"}>
+            {piece}
+        </button>
+    );
+}
+
+class Board extends React.Component {
+
+    range = (start, stop, step = 1) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
+
+    renderSquare(row, column, cell) {
+        return (
+            <Square 
+            piece={this.props.squares[cell]} 
+            row={row}
+            column={column} />
+        );
+    }
+    
+    render() {
+        const {width, height} = this.props;
+        return (
+            <div>
+            {
+                this.range(0, height - 1).map(row => (
+                    <div className="board-row">
+                        {
+                            this.range(row * width, row * width + width - 1)
+                                .map((cell, column) => this.renderSquare(row, column, cell))
+                        }
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+
+    constructor() {
+        super();
+        const width = CONSTANTS.BOARD_WIDTH;
+        const height = CONSTANTS.BOARD_HEIGHT;
+        const armyRows = CONSTANTS.ARMY_ROWS;
+        const initialBoard = 
+            [...Array(width * armyRows).fill('R'),
+            ...Array(width * height - 2 * width * armyRows).fill(null),
+            ...Array(width * armyRows).fill('B')
+            ];
+        this.state = {
+            squares: initialBoard,
+        }
+    }
+    
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board 
+                        squares = {this.state.squares} 
+                        width = {CONSTANTS.BOARD_WIDTH}
+                        height = {CONSTANTS.BOARD_HEIGHT}
+                        armyRows = {CONSTANTS.ARMY_ROWS} />
+                </div>
+                <div className="game-info">
+                    "Placeholder Text"
+                </div>
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render (
+    <Game />,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function calculateWinner(squares) {
+}
